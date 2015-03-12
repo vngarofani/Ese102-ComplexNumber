faa87844617b99050df9547c434179c1b2f37036
Ivan Garofani <ivan.garofani@gmail.com>
2015-03-13 00:29:20 +0100

    second commit

--- src/Ese102/ComplexNumber.java
+++ src/Ese102/ComplexNumber.java
@@ -3,22 +3,67 @@ package Ese102;
 
 
 public class ComplexNumber {
-    
+   
+   static private double initRe;
+   static private double initIm; 
+    private static StringFormat initFormat;
+
    private double re,im;
    
+    static public enum StringFormat {
+		RECTANGULAR,
+		POLAR, RETTANGOLARI, POLARI
+	}
+   
+   private StringFormat format;
+   
+   public ComplexNumber(){
+		this.re=ComplexNumber.initRe;
+		this.im=ComplexNumber.initIm;
+		this.format=ComplexNumber.initFormat; 
+	}
+   
+   private static double normalize(double a) {
+		a += (a<0)?2*Math.PI:0;
+		a = a % (2*Math.PI);
+		return a;
+	}
+   
+   static public void setInitRectangular(double re, double im) {
+		
+		initRe = re;
+		initIm = im;
+		initFormat= StringFormat.RETTANGOLARI; //setto il formato delle successive visualizzazioni a RETTANGOLARE
+	}		
+		
+   static public void setInitPolar(double modulus, double argument) {
+	
+                
+		argument = normalize(argument);
+		initRe = modulus * (Math.cos(argument * Math.PI / 180));
+		initIm = modulus * (Math.sin(argument * Math.PI / 180));
+		
+		
+		initFormat = StringFormat.POLARI; 
+	}
+   
+   
+   
+   
+    
    double getRe() {
        return re;
-   }
+    }
    
    double getIm() {
        return im;
     }
    
-   double getMod() {
+   double getModulus() {
        return Math.sqrt((this.re * this.re) + (this.im * this.im));
    }
    
-   double getArg() {
+   double getArgument() {
        double r = 0 ;
        
        if (re==0 && im==0) {
@@ -38,31 +83,24 @@ public class ComplexNumber {
        
        return r;
    }
-       
-       
-           
-       
- 
-   
-   
-           
-   public String ComplexNumbCartesiano() {
+              
+   public String formatComplexNumbRettangular() {
        String n  = this.getRe() + "+(" + this.getIm() + ")i";
        return n;
    }
    
-   public String ComplexNumbPolare() {
-       String n = "Modulo " + this.getMod() + " Argomento " + this.getArg();
+   public String formatComplexNumbPolar() {
+       String n = "Modulo " + this.getModulus() + " Argomento " + this.getArgument();
        
        return n;
    }
 
-    void setRectangular(double re, double im) {
+   void setRectangular(double re, double im) {
 		this.re = re;
 		this.im = im;
 	}
     
-    void setPolar(double Modulus, double Argument) {
+   void setPolar(double Modulus, double Argument) {
 
 		if (Argument == 90 || Argument == 270)
 			this.re = 0;
@@ -75,4 +113,91 @@ public class ComplexNumber {
 
         }
     
+   public ComplexNumber add(ComplexNumber operand) {
+		
+		ComplexNumber somma = new ComplexNumber();
+		double re = this.re + operand.getRe();
+		double im = this.im + operand.getIm();
+		somma.setRectangular(re, im);
+		return somma;
+	}
+        
+   public ComplexNumber sub(ComplexNumber operand) {
+		
+		ComplexNumber differenza = new ComplexNumber();
+		double re = this.re - operand.getRe();
+		double im = this.im - operand.getIm();
+		differenza.setRectangular(re, im);
+		return differenza;
+	}
+    
+   public ComplexNumber multiply(ComplexNumber operand) {
+		
+		ComplexNumber prodotto = new ComplexNumber();
+		double modulo = this.getModulus() * operand.getModulus();
+		double argomento = this.getArgument() + operand.getArgument();
+		prodotto.setPolar(modulo, argomento);
+		return prodotto;
+        }  
+    
+   public ComplexNumber divide(ComplexNumber operand) {
+		
+		ComplexNumber risultato = new ComplexNumber();
+		if (operand.getModulus() != 0) {
+			double modulo = this.getModulus() / operand.getModulus();
+			double argomento = this.getArgument() - operand.getArgument();
+			risultato.setPolar(modulo, argomento);
+		} else
+			throw new IllegalArgumentException("Il modulo del divisore deve essere diverso da 0");
+		return risultato;
+	}
+    
+   public void setStringFormat(StringFormat format) {
+		this.format = format;
+	}
+   
+   public ComplexNumber getConjugate() {
+		ComplexNumber r = new ComplexNumber();
+		r.setRectangular(this.re, -this.im);
+		return r;
+	}
+   
+   @Override
+    public String toString() {
+		
+		return toString(this.format);
+	}
+        
+    public String toString(StringFormat format) {
+		
+		String r = new String();
+		switch(format) {
+		case RECTANGULAR:
+			r = this.re + "+(" + this.im + ")i";
+			break;
+		case POLAR:
+			r = this.getModulus() + "*exp(i*" + this.getArgument() +")";
+			break;
+		default:
+			throw new UnsupportedOperationException();
+		}
+		return r;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		boolean r = false;
+		if(o instanceof ComplexNumber) {
+			ComplexNumber p = (ComplexNumber)o;
+			if(this.re == p.getRe() && this.im == p.getIm()) {
+				r = true;
+			}
+		}
+		return r;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int)this.re * 1000 + (int)this.im * 1000;
+	}   
 }   
--- src/Ese102/Ese102.java
+++ src/Ese102/Ese102.java
@@ -1,100 +1,46 @@
-
-package Ese102;
-
-import java.io.*;
+package ese102;
 
 public class Ese102 {
 
-   
-    public static void main(String[] args) throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader tastiera = new BufferedReader(input);
-        String linea = "";
-        ComplexNumber numeroComplesso = new ComplexNumber();
-        int scelta = 0;
-        boolean valoreValido = false; 
-	        
-	double parteReale = 0;
-	double parteImmaginaria = 0;
-	        
-	double argomento=0; 
-	double modulo=0;
-        
-                while (scelta!=1 && scelta!=2) {
-	            System.out.println("tipo di coordinate:");
-	            System.out.println("1 Rettangolari ; 2 Polari");
-	            linea = tastiera.readLine();
-	            try {
-	                scelta = Integer.valueOf(linea).intValue();
-	            } catch (NumberFormatException e) {
-	                System.out.println("Scelta non valida, inserisci un numero");
-	            }
-	        }
-	        
-	        if(scelta==1){
-	            
-	            while (valoreValido == false) {
-	                System.out.print("parte REALE ");
-	                linea = tastiera.readLine();
-	                try {
-	                    parteReale = Double.valueOf(linea).doubleValue();
-	                    valoreValido = true;
-	                } catch (NumberFormatException e) {
-	                    System.out.println("Numero non valido.");
-	                }
-	            }
-	            valoreValido = false;
-	            while (valoreValido == false) {
+	public static void main(String[] args) {
+		ComplexNumber.setInitRectangular(0, 0);
+		ComplexNumber c1 = new ComplexNumber();
+		ComplexNumber.setInitRectangular(1, 1);
+		ComplexNumber c2 = new ComplexNumber();
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		System.out.println("Addiction: " + c1.add(c2));
+		ComplexNumber r = c1.add(c2);
+		r.setStringFormat(ComplexNumber.StringFormat.POLAR);
+		System.out.println("Addiction (polar): " + r);
+		System.out.println("Subtraction: " + c1.sub(c2));
+		
+		System.out.println("Subtraction (polar): " + c1.sub(c2));
+		
+		
+		ComplexNumber numeri[] = new ComplexNumber[4];
+		ComplexNumber.setInitRectangular(1, -2);
+		numeri[0] = new ComplexNumber();
+		numeri[1] = new ComplexNumber();
+		numeri[2] = numeri[1];
+		numeri[3] = new ComplexNumber();
+		numeri[3].setRectangular(-3, 2);
+		int i=0;
+		for(i=1; i<numeri.length; i++) {
+			if(numeri[i-1] == numeri[i]) {
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] si riferiscono alla medesima istanza");
+			}
+			else {
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] si riferiscono a due istanze differenti");
+			}
+		
+			if(numeri[i-1].equals(numeri[i])) {
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] contengono il medesimo numero complesso");
+			}
+			else {
+				System.out.println("numeri[" + (i-1) + "] e numeri[" + i + "] contengono numeri complessi diversi");
+			}
+		}
+	}
 
-	                System.out.print("parte IMMAGINARIA ");
-	                linea = tastiera.readLine();
-	                try {
-	                    parteImmaginaria = Double.valueOf(linea).doubleValue();
-	                    valoreValido = true;
-	                } catch (NumberFormatException e) {
-
-	                    System.out.println("Numero non valido.");
-	                }
-	            }
-	           numeroComplesso.setRectangular(parteReale, parteImmaginaria);
-	            System.out.println("Hai inserito il numero " + numeroComplesso.ComplexNumbCartesiano());
-	            System.out.println("Rappresenta un vettore di modulo " + numeroComplesso.getMod());
-	            System.out.println("Disegna un angolo Theta di " + numeroComplesso.getArg()+"�");
-	            
-	        }
-	            
-	        
-
-	        else if(scelta==2){
-	            while (valoreValido == false) {
-	                System.out.print("Inserisci il MODULO ");
-	                linea = tastiera.readLine();
-	                try {
-	                    modulo = Double.valueOf(linea).doubleValue();
-	                    valoreValido = true;
-	                } catch (NumberFormatException e) {
-	                    System.out.println("Numero non valido.");
-	                }
-	            }
-	            valoreValido = false;
-	            while (valoreValido == false) {
-
-	                System.out.print("Inserisci THETA ");
-	                linea = tastiera.readLine();
-	                try {
-	                    argomento = Double.valueOf(linea).doubleValue();
-	                    valoreValido = true;
-	                } catch (NumberFormatException e) {
-
-	                    System.out.println("Numero non valido.");
-	                }
-	            }
-	            numeroComplesso.setPolar(modulo, argomento);
-	            System.out.println("Hai inserito "+ numeroComplesso.ComplexNumbPolare());
-	            System.out.println("La sua componente REALE e' "+ numeroComplesso.getRe());
-	            System.out.println("La sua componente IMMAGINARIA e' "+ numeroComplesso.getIm());
-	        }
-    
-        }
-    
 }
